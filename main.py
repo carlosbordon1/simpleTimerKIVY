9cb4943ffd28f3c1a4c0939737611a4a4480e0d2
Bordon Laptop <soycarlosbordon@gmail.com>
2026-06-21 22:54:12 -0400

    simple front

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.utils import get_color_from_hex
 
+class WindowManager(ScreenManager):
+    pass
 
+class PantallaInicial(Screen):
+    pass
+
+class PantallaAyuda(Screen):
+    pass
 # Clase que representa el layout
 class BoxFondo(BoxLayout):
     pass
@@ -10,9 +19,26 @@ class BoxFondo(BoxLayout):
 # Clase principal de la aplicación
 class Aplicacion(App):
     def build(self):
+        # Convertir HEX a [R,G,B,A]
+        self.mi_color_hex = get_color_from_hex("#B518A0") 
+        self.a = True
+
+
+
         # Carga la definición KV
         return Builder.load_file("componentes/definicionesKV.kv")
-    
+        
+    def cambiar_pantalla(self, pantalla_destino):
+        # Cambia a la pantalla especificada
+        if self.a == True: 
+            self.root.transition.direction = "left" 
+            self.a = False
+        else:
+            self.root.transition.direction = "right" 
+            self.a = True
+
+        self.root.current = pantalla_destino
+
     # Metodo para cerrar la app correctamente
     def cerrar_aplicacion(self, instance):
         # Detiene la aplicación de forma segura
